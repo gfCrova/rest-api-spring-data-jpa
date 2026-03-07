520ee2554e21f8d092c4b267d4d21f347e65ac6e
gfCrova <giancrova.cl5@gmail.com>
2026-03-07 02:00:20 -0300

    adding new proyect Spring REST to git repository

---
+++ src/main/java/com/demo/obspringrestfuldatajpa/ObSpringResTfulDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.obspringrestfuldatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringResTfulDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObSpringResTfulDataJpaApplication.class, args);
+    }
+
+}
