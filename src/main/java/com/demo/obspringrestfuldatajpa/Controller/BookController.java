dcef4f2fd03d0d5d668d4b2fa20686de696b1139
gfCrova <giancrova.cl5@gmail.com>
2026-03-07 21:22:41 -0300

    new modifieds, in controller add put and delete methods.

--- src/main/java/com/demo/obspringrestfuldatajpa/Controller/BookController.java
+++ src/main/java/com/demo/obspringrestfuldatajpa/Controller/BookController.java
@@ -2,15 +2,21 @@ package com.demo.obspringrestfuldatajpa.Controller;
 
 import com.demo.obspringrestfuldatajpa.Entities.Book;
 import com.demo.obspringrestfuldatajpa.Repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
 
     // Atributo de inyección de BookRepository
     private final BookRepository bookRepository;
+    // Logger para el control de errores y mensajes de la aplicación
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
     // Inyecta el bean de BookRepository y asi accedemos a los métodos de la interface
     public BookController(BookRepository bookRepository) {
@@ -23,6 +29,7 @@ public class BookController {
      */
     @GetMapping("api/books")
     public List<Book> findAlls() {
+       log.info("200 OK - Getting all books");
        return bookRepository.findAll();
     }
 
@@ -32,13 +39,73 @@ public class BookController {
      * @return Book by Id
      */
     @GetMapping("api/books/{id}")
-    public Book findById(@PathVariable Long id) {  // -> @PathVariable: Indica que el parámetro id es un valor de la URL
-       return bookRepository.findById(id).orElse(null);
+    // ResponseEntity - Sirve para devolver un objeto y un estado HTTP
+    public ResponseEntity<Book> findById(@PathVariable Long id) {  // -> @PathVariable: Indica que el parámetro id es un valor de la URL
+        log.info("200 OK - Getting book by id: {}", id);
+        Optional<Book> book = bookRepository.findById(id);
+        return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // Metodo POST para crear un libro
-    @PostMapping("api/books/create")
-    public Book create(@RequestBody Book book) {  // -> @RequestBody: Indica que el parámetro book es un valor de la URL
-       return bookRepository.save(book);
+    /**
+     * @param book
+     * @return Book created
+     */
+    // Metodo POST para CREAR un libro y guardarlo en la base de datos
+    @PostMapping("api/books")
+    public ResponseEntity<Book> create(@RequestBody Book book) {  // -> @RequestBody: Indica que el parámetro book es un valor de la URL
+        if (book.getId() != null){
+            log.error("400 Bad Request - The book ID must be null to create a new book");
+            return ResponseEntity.badRequest().build(); // Si existe un id, no se puede crear
+        }
+        log.info("201 Created - Book created successfully");
+        Book savedBook = bookRepository.save(book);
+        return ResponseEntity.ok(savedBook);
     }
+
+    // Metodo PUT para ACTUALIZAR/MODIFICAR un libro en base de datos
+    @PutMapping("api/books")
+    public ResponseEntity<Book> update(@RequestBody Book book){
+        // Si el id es null, no se puede actualizar
+        if (book.getId() == null){
+            log.error("400 Bad Request - The book ID must not be null in order to update a book");
+            return ResponseEntity.badRequest().build();
+        }
+        // Si el libro no existe, no se puede actualizar
+        if (!bookRepository.existsById(book.getId())){
+            log.error("404 Not Found - The book ID does not exist in order to update a book");
+            return ResponseEntity.notFound().build();
+        }
+        log.info("200 OK - Book updated successfully");
+        Book savedBook = bookRepository.save(book);
+        return ResponseEntity.ok(savedBook);
+    }
+
+    /**
+     * @Param id
+     * @return Book deleted
+     */
+    // Metodo DELETE para borrar un libro de la base de datos
+    @DeleteMapping("api/books/{id}")
+    public ResponseEntity<Book> delete(@PathVariable Long id){
+        if (!bookRepository.existsById(id)){
+            log.error("404 Not Found - The book ID does not exist and cannot be deleted.");
+            return ResponseEntity.notFound().build();
+        }
+        bookRepository.deleteById(id);
+        log.info("204 No Content - Book deleted successfully");
+        return ResponseEntity.noContent().build();
+    }
+
+    // Metodo DELETE para borrar todos los libros
+    @DeleteMapping("api/books")
+    public ResponseEntity<Void> deleteAll(){
+        if (bookRepository.findAll().isEmpty()){
+            log.error("404 Not Found - There are no books to delete..");
+            return ResponseEntity.notFound().build();
+        }
+        log.info("204 No Content - All books deleted");
+        bookRepository.deleteAll();
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/demo/obspringrestfuldatajpa/ObSpringResTfulDataJpaApplication.java
+++ src/main/java/com/demo/obspringrestfuldatajpa/ObSpringResTfulDataJpaApplication.java
@@ -25,5 +25,4 @@ public class ObSpringResTfulDataJpaApplication {
 
         System.out.println(bookRepository.findByOnline(true));
     }
-
 }
