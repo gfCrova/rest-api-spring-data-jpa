a67798d6c300be2886d49b22a067969b70500aeb
gfCrova <giancrova.cl5@gmail.com>
2026-03-08 20:52:36 -0300

    Modifying HTTP methods, annotations, and documentation with Swagger

--- src/main/java/com/demo/obspringrestfuldatajpa/Controller/BookController.java
+++ src/main/java/com/demo/obspringrestfuldatajpa/Controller/BookController.java
@@ -1,9 +1,10 @@
 package com.demo.obspringrestfuldatajpa.Controller;
 
 import com.demo.obspringrestfuldatajpa.Entities.Book;
-import com.demo.obspringrestfuldatajpa.Repository.BookRepository;
+import com.demo.obspringrestfuldatajpa.Service.BookService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,107 +14,124 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
 
-    // Atributo de inyección de BookRepository
-    private final BookRepository bookRepository;
+    // Atributo de inyección de BookService
+    private final BookService bookService;
+
+    // Inyecta el bean de BookRepository y asi accedemos a los métodos de la interface
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     // Logger para el control de errores y mensajes de la aplicación
     private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
-    // Inyecta el bean de BookRepository y asi accedemos a los métodos de la interface
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
     /**
-     * <a href="http://localhost:8080/api/books">http://localhost:8080/api/books</a>
-     * @return List of all Books
+     * {@code @Param} pageable
+     * {@code @Return} List of all Books
      */
     @Operation(summary = "List alls books", description = "It retrieves a list of all books registered in the database.")
-    @GetMapping("api/books")
-    public List<Book> findAlls() {
+    @GetMapping
+    public ResponseEntity<List<Book>> findAlls(Pageable pageable) {
        log.info("200 OK - Getting all books");
-       return bookRepository.findAll();
+       Page<Book> books = bookService.findAllsBooks(pageable);
+       return ResponseEntity.ok(books.getContent());
     }
 
     /**
-     * <a href="http://localhost:8080/api/books/{id}">http://localhost:8080/api/books/{id}</a>
-     * @param id
-     * @return Book by Id
+     * {@code @Param} id
+     * {@code @return} Book by Id
      */
     @Operation(summary = "Get a book by your ID", description = "Returns a book based on the provided ID")
-    @GetMapping("api/books/{id}")
+    @GetMapping("/{id}")
     // ResponseEntity - Sirve para devolver un objeto y un estado HTTP
     public ResponseEntity<Book> findById(@PathVariable Long id) {  // -> @PathVariable: Indica que el parámetro id es un valor de la URL
+        Optional<Book> book = bookService.findBookById(id);
+        if (book.isEmpty()){
+            log.error("404 Not Found - The book ID does not exist and cannot be obtained.");
+            return ResponseEntity.notFound().build();
+        }
         log.info("200 OK - Getting book by id: {}", id);
-        Optional<Book> book = bookRepository.findById(id);
         return book.map(ResponseEntity::ok)
                     .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     /**
-     * @param book
-     * @return Book created
+     * {@code @Param} book
+     * {@code @Return} Book created
      */
     // Metodo POST para CREAR un libro y guardarlo en la base de datos
     @Operation(summary = "Create book", description = "Create a new book and save it to the database")
-    @PostMapping("api/books")
+    @PostMapping
     public ResponseEntity<Book> create(@RequestBody Book book) {  // -> @RequestBody: Indica que el parámetro book es un valor de la URL
         if (book.getId() != null){
             log.error("400 Bad Request - The book ID must be null to create a new book");
             return ResponseEntity.badRequest().build(); // Si existe un id, no se puede crear
         }
         log.info("201 Created - Book created successfully");
-        Book savedBook = bookRepository.save(book);
+        Book savedBook = bookService.createBook(book);
         return ResponseEntity.ok(savedBook);
     }
 
+    /**
+     * {@code @Param} book
+     * {@code @Return} Update Book
+     */
     // Metodo PUT para ACTUALIZAR/MODIFICAR un libro en base de datos
     @Operation(summary = "Update book", description = "Update the data of an existing book")
-    @PutMapping("api/books")
+    @PutMapping
     public ResponseEntity<Book> update(@RequestBody Book book){
+        Optional<Book> existingBook = bookService.findBookById(book.getId());
         // Si el id es null, no se puede actualizar
         if (book.getId() == null){
             log.error("400 Bad Request - The book ID must not be null in order to update a book");
             return ResponseEntity.badRequest().build();
         }
         // Si el libro no existe, no se puede actualizar
-        if (!bookRepository.existsById(book.getId())){
+        if (existingBook.isEmpty()){
             log.error("404 Not Found - The book ID does not exist in order to update a book");
             return ResponseEntity.notFound().build();
         }
         log.info("200 OK - Book updated successfully");
-        Book savedBook = bookRepository.save(book);
+        Book savedBook = bookService.updateBook(book);
         return ResponseEntity.ok(savedBook);
     }
 
     /**
-     * @Param id
-     * @return Book deleted
+     * {@code @Param} id
+     * {@code @Return} Book deleted
      */
     // Metodo DELETE para borrar un libro de la base de datos
     @Operation(summary = "Delete a book by your ID", description = "Delete a book of the database")
-    @DeleteMapping("api/books/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Book> delete(@PathVariable Long id){
-        if (!bookRepository.existsById(id)){
+        Optional<Book> book = bookService.findBookById(id);
+        if (book.isEmpty()){
             log.error("404 Not Found - The book ID does not exist and cannot be deleted.");
             return ResponseEntity.notFound().build();
         }
-        bookRepository.deleteById(id);
+        bookService.deleteBookById(id);
         log.info("204 No Content - Book deleted successfully");
         return ResponseEntity.noContent().build();
     }
 
+    /**
+     * {@code @Param} pageable
+     * {@code @Return} no content
+     */
     // Metodo DELETE para borrar todos los libros
     @Operation(summary = "Delete alls books", description = "Delete alls book of the database")
-    @DeleteMapping("api/books")
-    public ResponseEntity<Void> deleteAll(){
-        if (bookRepository.findAll().isEmpty()){
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAll(Pageable pageable){
+        Page<Book> pageBook = bookService.findAllsBooks(pageable);
+        if (pageBook.isEmpty()){
             log.error("404 Not Found - There are no books to delete..");
             return ResponseEntity.notFound().build();
         }
         log.info("204 No Content - All books deleted");
-        bookRepository.deleteAll();
+        bookService.deleteAllsBooks();
         return ResponseEntity.noContent().build();
     }
 
