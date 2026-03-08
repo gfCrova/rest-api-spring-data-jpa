5ca43b16ad14a62ee664589d6a324d79348372d6
gfCrova <giancrova.cl5@gmail.com>
2026-03-08 20:58:46 -0300

    create new context bookService

--- src/main/java/com/demo/obspringrestfuldatajpa/ObSpringResTfulDataJpaApplication.java
+++ src/main/java/com/demo/obspringrestfuldatajpa/ObSpringResTfulDataJpaApplication.java
@@ -2,6 +2,7 @@ package com.demo.obspringrestfuldatajpa;
 
 import com.demo.obspringrestfuldatajpa.Entities.Book;
 import com.demo.obspringrestfuldatajpa.Repository.BookRepository;
+import com.demo.obspringrestfuldatajpa.Service.BookService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
@@ -24,14 +25,15 @@ public class ObSpringResTfulDataJpaApplication {
 
         ApplicationContext context = SpringApplication.run(ObSpringResTfulDataJpaApplication.class, args);
         BookRepository bookRepository = context.getBean(BookRepository.class);
+        BookService bookService = context.getBean(BookService.class);
 
         Book book1 = new Book("Stephen King", true, LocalDate.of(1987,11,14), 540, 250.5, "Titanic");
         Book book2 = new Book("J.K. Rowling", true, LocalDate.of(2001,6,20), 540, 416.0, "El señor de los anillos");
         Book book3 = new Book("F. Scott Fitzgerald", true, LocalDate.of(2013,2,27), 1165, 850.5, "La novia");
 
-        bookRepository.save(book1);
-        bookRepository.save(book2);
-        bookRepository.save(book3);
+        bookService.createBook(book1);
+        bookService.createBook(book2);
+        bookService.createBook(book3);
 
         System.out.println(bookRepository.findByOnline(true));
     }
