e0d1595997e03f54b9db99c216a6f702b98c8e55
gfCrova <giancrova.cl5@gmail.com>
2026-03-08 20:58:06 -0300

    modifying repository

--- src/main/java/com/demo/obspringrestfuldatajpa/Repository/BookRepository.java
+++ src/main/java/com/demo/obspringrestfuldatajpa/Repository/BookRepository.java
@@ -10,8 +10,6 @@ import java.util.List;
  */
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
     // -> findByOnline: Busca libros por su estado online
-    public List<Book> findByOnline(boolean online);
-
+    List<Book> findByOnline(boolean online);
 }
